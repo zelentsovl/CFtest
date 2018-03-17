20ea7bf665b5d67ac321d2e34ad31929c6e99a9f
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-17 16:48:36 +0000

    fix syntax in report

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 source ~/.nvm/nvm.sh && 
 nvm install --lts && 
 ~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
-echo "<html><head></head><body><font size='18'" > report.html
+echo "<html><head></head><body><font size='18'>" > report.html
 ~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate ./Wordpress_CF_template.json >> report.html
 echo "</font></body></html>" >> report.html
