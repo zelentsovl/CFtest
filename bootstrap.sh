778bb9cee52001a93358a243a8a7a6c4faaa1ca6
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-17 16:34:17 +0000

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && 
+source ~/.nvm/nvm.sh && 
+nvm install --lts && 
+~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
+echo "<html><head></head><body><font size='18'" > report.html
+~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate ./Wordpress_CF_template.json >> report.html
+echo "</font></body></html>" >> report.html
