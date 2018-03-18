376788db44f7b95a33eb99c38059f9c54d91b431
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-18 14:06:36 +0000

    fix cf-lint syntax

--- cf-lint-test.sh
+++ cf-lint-test.sh
@@ -5,5 +5,5 @@ nvm install --lts &&
 ~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
 mkdir cf_lint_outputs
 # echo "<html><head>Template validation report: </font></head><body>" > ./cf_lint_outputs/validation-report.html
-~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate $1  >> ./cf_lint_outputs/validation-report.log
+~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate $1  &>> ./cf_lint_outputs/validation-report.log
 # echo "</body></html>" >> ./cf_lint_outputs/validation-report.html
