9d46dd10f4c4afc5899c3d369a16ded7334960b4
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-18 12:49:54 +0000

    add cf-lint-test script

---
+++ cf-lint-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && 
+source ~/.nvm/nvm.sh && 
+nvm install --lts && 
+~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
+mkdir cf_lint_outputs
+echo "<html><head>Template validation report: </font></head><body>" > ./cf_lint_outputs/validation-report.html
+~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate $1  | sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' |& tee -a ./cf_lint_outputs/validation-report.html
+echo "</body></html>" >> ./cf_lint_outputs/validation-report.html
