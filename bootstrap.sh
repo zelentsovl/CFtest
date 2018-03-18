31896de4b9f3bee1db929011c1f5833323d3d936
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-18 11:56:12 +0000

    fix syntax in bootstarp

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ source ~/.nvm/nvm.sh &&
 nvm install --lts && 
 ~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
 echo "<html><head>Template validation report</head><body><font size='14'>" > validation-report.html
-~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate ./audi-cf-test/Wordpress_CF_template.json  2>&1 | sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' | tee -a validation-report.html
+~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate ./audi-cf-test/Wordpress_CF_template.json  | sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' |& tee -a validation-report.html
 echo "</font></body></html>" >> validation-report.html
