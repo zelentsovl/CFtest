26045901a115d0ebe216f54cf52c041a3b8230c3
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-18 12:46:18 +0000

    rename cf-lint launch script and remove font from report

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash && 
-source ~/.nvm/nvm.sh && 
-nvm install --lts && 
-~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
-mkdir cf_lint_outputs
-echo "<html><head><font size='12'>Template validation report:/n</font></head><body><font size='12'>" > ./cf_lint_outputs/validation-report.html
-~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate $1  | sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' |& tee -a ./cf_lint_outputs/validation-report.html
-echo "</font></body></html>" >> ./cf_lint_outputs/validation-report.html
