f2f1a4958c15e95ecfa9dd47c92568544de1e31f
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-18 14:26:50 +0000

    adddeliting report folders before creation

--- cf-lint-test.sh
+++ cf-lint-test.sh
@@ -3,7 +3,8 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 source ~/.nvm/nvm.sh && 
 nvm install --lts && 
 ~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
-mkdir cf_lint_outputs
+rm -rf ./cf_lint_outputs
+mkdir ./cf_lint_outputs
 # echo "<html><head>Template validation report: </font></head><body>" > ./cf_lint_outputs/validation-report.html
 ~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate $1  &>> ./cf_lint_outputs/validation-report.log
 # echo "</body></html>" >> ./cf_lint_outputs/validation-report.html
--- taskcat-test.sh
+++ taskcat-test.sh
@@ -1,2 +1,3 @@
+rm -rf ./taskcat_outputs
 mkdir ./taskcat_outputs
 /usr/local/bin/taskcat -c ./cf-template/config.yml | sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' |& tee -a ./taskcat_outputs/taskcat-exec-report.log
