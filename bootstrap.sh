35ed8ef225ed84f05cae5dfd27eb36aba6bab44e
EC2 Default User <ec2-user@ip-172-31-22-72.eu-central-1.compute.internal>
2018-03-18 12:24:13 +0000

    add folder cf_lint_outputs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | b
 source ~/.nvm/nvm.sh && 
 nvm install --lts && 
 ~/.nvm/versions/node/v8.10.0/bin/npm install -g cfn-lint && 
-echo "<html><head>Template validation report</head><body><font size='14'>" > validation-report.html
-~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate ./audi-cf-test/Wordpress_CF_template.json  | sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' |& tee -a validation-report.html
-echo "</font></body></html>" >> validation-report.html
+mkdir cf_lint_outputs
+echo "<html><head><font size='12'>Template validation report:/n</font></head><body><font size='12'>" > ./cf_lint_outputs/validation-report.html
+~/.nvm/versions/node/v8.10.0/bin/cfn-lint validate $1  | sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' |& tee -a ./cf_lint_outputs/validation-report.html
+echo "</font></body></html>" >> ./cf_lint_outputs/validation-report.html
